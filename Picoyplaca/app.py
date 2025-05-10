6ddee340f89367c8421ef52af4ab6835f8619096
SaidNa14-02 <msnavarretem@puce.edu.ec>
2025-05-10 13:42:56 -0500

    Created forms, and added styles with css to make an interface

--- Picoyplaca/app.py
+++ Picoyplaca/app.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template
+from forms.pico_placa_form import PicoPlacaForm
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'trial1'
+app.config['SECRET_KEY'] = 'trial1' 
 
-@app.route('/')
-
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    
-    return render_template('index.html')
+    form = PicoPlacaForm()
+    return render_template('index.html', form=form)
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
---
+++ Picoyplaca/forms/pico_placa_form.py
@@ -0,0 +1,29 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, ValidationError
+from models.vehicle import Vehicle
+from models.schedule import Schedule
+
+class PicoPlacaForm(FlaskForm):
+    plate = StringField('License Plate', validators=[DataRequired()])
+    date = StringField('Date', validators=[DataRequired()])
+    time = StringField('Time', validators=[DataRequired()])
+    submit = SubmitField('Check Restriction')
+
+    def __init__(self, *args, **kwargs):
+        super(PicoPlacaForm, self).__init__(*args, **kwargs)
+        self.schedule = Schedule()
+
+    def validate_plate(self, field):
+        try:
+            vehicle = Vehicle(field.data)
+        except ValueError:
+            raise ValidationError('Invalid plate format. Must be ABC-1234')
+
+    def validate_date(self, field):
+        if not self.schedule.check_date(field.data):
+            raise ValidationError('Invalid date format. Must be DD/MM/YYYY')
+
+    def validate_time(self, field):
+        if not self.schedule.check_time(field.data):
+            raise ValidationError('Invalid time format. Must be HH:MM')
