fb71fcb18e4bf97fdc45c661a486e29d1cb45482
SaidNa14-02 <msnavarretem@puce.edu.ec>
2025-05-10 12:14:53 -0500

    Added time and day restriction modules to manage the pico y placa rules

---
+++ Picoyplaca/models/day_restriction.py
@@ -0,0 +1,36 @@
+import calendar
+from models.schedule import Schedule
+from models.vehicle import Vehicle
+
+class DayRestriction:
+    RESTRICTION_DAYS = {
+        "Monday": [1, 2],
+        "Tuesday": [3, 4],
+        "Wednesday": [5, 6],
+        "Thursday": [7, 8],
+        "Friday": [9, 0]
+    }
+
+    def __init__(self):
+        self.schedule = Schedule()
+
+    def get_day_of_week(self, date: str):
+        if not self.schedule.check_date(date):
+            raise ValueError("Invalid date format")
+        
+        day, month, year = map(int, date.split("/"))
+        weekday = calendar.weekday(year, month, day)
+        return calendar.day_name[weekday]
+
+    def check_day_restriction(self, input_date: str, plate: str):
+        if not self.schedule.check_date(input_date):
+            raise ValueError("Invalid date format")
+
+        temp_vehicle = Vehicle(plate)
+        if not temp_vehicle.check_plate(plate):
+            raise ValueError("Invalid plate format")
+
+        day_name = self.get_day_of_week(input_date)
+        last_digit = int(plate[-1])
+        
+        return last_digit in self.RESTRICTION_DAYS[day_name]
---
+++ Picoyplaca/models/time_restriction.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+from models.schedule import Schedule
+
+class TimeRestriction:
+    RESTRICTION_HOURS = {
+        "Morning": (datetime.strptime("07:00", "%H:%M").time(), datetime.strptime("09:00", "%H:%M").time()),
+        "Afternoon": (datetime.strptime("16:00", "%H:%M").time(), datetime.strptime("19:30", "%H:%M").time()),
+    }
+    def __init__(self):
+        self.schedule = Schedule()
+
+    def check_time_restriction(self, input_time): 
+        if not self.schedule.check_time(input_time):
+            raise ValueError("Invalid time format")
+        
+        input_time = datetime.strptime(input_time, "%H:%M").time()
+
+        for value in self.RESTRICTION_HOURS.values():
+            if value[0] <= input_time <= value[1]:
+                return True
+        return False
+
+
+
